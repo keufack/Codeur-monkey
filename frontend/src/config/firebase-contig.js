40f7a4a9942b3828450bd7c1bd78d9118a0bb315
keufack <ngimdockkeufack@gmail.com>
2023-09-22 18:15:38 +0100

    initial commit

---
+++ frontend/src/config/firebase-contig.js
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC__rSUH0Zji6pN7xtdSMAyn41B-BWjaQU",
+  authDomain: "codeur-monkey-2.firebaseapp.com",
+  projectId: "codeur-monkey-2",
+  storageBucket: "codeur-monkey-2.appspot.com",
+  messagingSenderId: "580792308146",
+  appId: "1:580792308146:web:9927665c64b72de53814ab",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { Seo } from "@/ui/components/Seo";
+
+export default function Home() {
+  return (
+    <>
+      <Seo title="Codeur-Monkey" description="Description..." />
+      <h1>Hello bg</h1>
+    </>
+  );
+}
