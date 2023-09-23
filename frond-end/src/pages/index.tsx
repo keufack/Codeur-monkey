176c37b3c085b3222f42982c3d007b501e4f91ec
keufack <ngimdockkeufack@gmail.com>
2023-09-23 11:37:52 +0100

    initial-commit

---
+++ frond-end/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Seo } from "@/ui/components/Seo";
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Seo title="Codeur-monkey" description="Description..." />
+      <h1>hello bg</h1>
+    </>
+  );
+}
